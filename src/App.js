469053cc0f520f75665eedf82d694835f646527c
Zolbayar <zolbayarq@gmail.com>
2019-03-07 15:11:37 +0800

    Displaying info from static list and the search

--- src/App.js
+++ src/App.js
@@ -2,24 +2,88 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const list = [
+    {
+      title: 'React',
+      url: 'https://reactjs.org/',
+      author: 'Jordan Walke',
+      num_comments: 3,
+      points: 4,
+      objectID: 0,
+    },
+    {
+      title: 'Redux',
+      url: 'https://redux.js.org/',
+      author: 'Dan Abramov, Andrew Clark',
+      num_comments: 2,
+      points: 5,
+      objectID: 1,
+    },
+  ]
+
+const isSearched = (searchTerm) => {
+  return (item) => {
+    return item.title.toLowerCase().includes(searchTerm.toLowerCase()); 
+  }
+}
+
 class App extends Component {
+
+  constructor(props){
+    super(props); 
+    
+    this.state = {
+      list,
+      searchTerm: '',
+    }
+
+    // this.onDismiss = this.onDismiss.bind(this);
+  }
+
+  onDismiss = (id) => {
+    const updatedList = this.state.list.filter((value) => {
+      return id != value.objectID; 
+    });
+
+    this.setState({ list: updatedList });
+  }
+
+  onSearchChange = (event) => {
+    this.setState({ searchTerm: event.target.value }); 
+  }
+
   render() {
+    const user = {
+      firstname: 'Z',
+      lastname: 'B',
+      age: 27
+    }
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <form>
+          <input 
+            type="text"
+            onChange={this.onSearchChange}
+          />
+        </form>
+        {this.state.list.filter(isSearched(this.state.searchTerm)).map((item) => 
+          <div key={item.objectID}>
+            <span>
+              <a href={item.url}>{item.title}</a>
+            </span>
+            <span>{item.author}</span>
+            <span>{item.num_comments}</span>
+            <span>{item.points}</span>
+            <span>
+              <button 
+                onClick={() => this.onDismiss(item.objectID)}
+                type="button"
+              >
+                Dismiss
+              </button>
+            </span>
+          </div>
+        )}
       </div>
     );
   }
