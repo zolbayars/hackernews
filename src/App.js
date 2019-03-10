bb713709a31990fb5985dd8d7984e8cc1f6f6e30
Zolbayar <zolbayarq@gmail.com>
2019-03-10 19:39:33 +0800

    Split up components

--- src/App.js
+++ src/App.js
@@ -31,12 +31,11 @@ class App extends Component {
 
   constructor(props){
     super(props); 
-    
+
     this.state = {
       list,
       searchTerm: '',
     }
-
     // this.onDismiss = this.onDismiss.bind(this);
   }
 
@@ -57,14 +56,42 @@ class App extends Component {
 
     return (
       <div className="App">
+        <Search 
+          searchTerm={searchTerm} 
+          onChange={this.onSearchChange} 
+        />
+        <Table 
+          list={list} 
+          pattern={searchTerm} 
+          onDismiss={this.onDismiss} 
+        />
+      </div>
+    );
+  }
+}
+
+class Search extends Component {
+  render() {
+    const { searchTerm, onChange } = this.props; 
+
+    return (
         <form>
           <input 
             type="text"
             value={searchTerm}
-            onChange={this.onSearchChange}
+            onChange={onChange}
           />
         </form>
-        {list.filter(isSearched(searchTerm)).map((item) => 
+    );
+  }
+}
+
+class Table extends Component {
+  render() {
+    const { list, pattern, onDismiss } = this.props; 
+    return (
+      <div>
+        {list.filter(isSearched(pattern)).map((item) => 
           <div key={item.objectID}>
             <span>
               <a href={item.url}>{item.title}</a>
@@ -74,7 +101,7 @@ class App extends Component {
             <span>{item.points}</span>
             <span>
               <button 
-                onClick={() => this.onDismiss(item.objectID)}
+                onClick={() => onDismiss(item.objectID)}
                 type="button"
               >
                 Dismiss
@@ -83,7 +110,7 @@ class App extends Component {
           </div>
         )}
       </div>
-    );
+    ); 
   }
 }
 
