98fabdb592b768ddae69fffb9cc152ef303e73a1
Zolbayar <zolbayarq@gmail.com>
2019-03-11 08:47:42 +0800

    Added css styling

--- src/App.js
+++ src/App.js
@@ -55,11 +55,13 @@ class App extends Component {
     const { searchTerm, list } = this.state; 
 
     return (
-      <div className="App">
-        <Search 
-          searchTerm={searchTerm} 
-          onChange={this.onSearchChange} 
-        >Search:</Search>
+      <div className="page">
+        <div className="interactions">
+          <Search 
+            searchTerm={searchTerm} 
+            onChange={this.onSearchChange} 
+          >Search:</Search>
+        </div>
         <Table 
           list={list} 
           pattern={searchTerm} 
@@ -81,17 +83,26 @@ const Search = ({ searchTerm, onChange, children }) =>
     
 
 const Table = ({ list, pattern, onDismiss }) => 
-  <div>
+  <div className="table">
     {list.filter(isSearched(pattern)).map((item) => 
-      <div key={item.objectID}>
-        <span>
+      <div key={item.objectID} className="table-row">
+        <span style={{ width: '40%' }}>
           <a href={item.url}>{item.title}</a>
         </span>
-        <span>{item.author}</span>
-        <span>{item.num_comments}</span>
-        <span>{item.points}</span>
-        <span>
-          <Button onClick={() => onDismiss(item.objectID)}>
+        <span style={{ width: '30%' }}>
+          {item.author}
+        </span>
+        <span style={{ width: '10%' }}>
+          {item.num_comments}
+        </span>
+        <span style={{ width: '10%' }}>
+          {item.points}
+        </span>
+        <span style={{ width: '10%' }}>
+          <Button 
+            onClick={() => onDismiss(item.objectID)}
+            className="button-inline"
+          >
             Dismiss
           </Button>
         </span>
