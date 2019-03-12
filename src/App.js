20e35622fa2844afe33192ced09d5cb37b6b7005
Zolbayar <zolbayarq@gmail.com>
2019-03-12 11:31:38 +0800

    Added snaptshot tests with Jest

--- src/App.js
+++ src/App.js
@@ -196,3 +196,9 @@ const Button = ({ onClick, className = '', children }) =>
     
 
 export default App;
+
+export {
+  Button,
+  Search,
+  Table,
+};
--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,87 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import renderer from 'react-test-renderer';
+import App, { Search, Button, Table } from './App';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+
+describe('App', () => {
+
+  it('renders without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<App />, div);
+    ReactDOM.unmountComponentAtNode(div);
+  });
+
+  test('has a valid snapshot', () => {
+    const component = renderer.create(
+      <App />
+    );
+    const tree = component.toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+
+}); 
+
+describe('Search', () => {
+
+  it('renders without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<Search>Search</Search>, div); 
+    ReactDOM.unmountComponentAtNode(div);
+  });
+
+  test('has a valid snapshot', () => {
+    const component = renderer.create(
+      <Search>Search</Search>
+    );
+    const tree = component.toJSON(); 
+    expect(tree).toMatchSnapshot(); 
+  });
+
 });
+
+describe('Button', () => {
+
+  it('renders without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<Button>Click on me</Button>, div); 
+    ReactDOM.unmountComponentAtNode(div);
+  });
+
+  test('has a valid snapshot', () => {
+    const component = renderer.create(
+      <Button>Click on me</Button>
+    );
+    const tree = component.toJSON(); 
+    expect(tree).toMatchSnapshot(); 
+  });
+
+});
+
+describe('Table', () => {
+
+  const props = {
+    list:[
+      { title: '1', author: '1', num_comments: 1, points: 2, objectID: 'y' },
+      { title: '2', author: '2', num_comments: 1, points: 2, objectID: 'z' },
+    ],
+  };
+  
+  it('renders without crashing', () => {
+    const div = document.createElement('div');
+    // ReactDOM.render(<Table { ...props } />, div); 
+    ReactDOM.render(<Table result={ props.list } />, div); 
+    ReactDOM.unmountComponentAtNode(div);
+  });
+
+  test('has a valid snapshot', () => {
+    const component = renderer.create(
+      // <Table { ...props } />
+      <Table result={ props.list } />
+    );
+    const tree = component.toJSON(); 
+    expect(tree).toMatchSnapshot(); 
+  });
+
+});
+
